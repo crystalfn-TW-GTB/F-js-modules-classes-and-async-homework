beaa42c36339b4bc225a6e4b87764ce311fd58ce
crystalfn <969274162@qq.com>
2020-11-10 23:57:18 +0800

    [fangni] feat: implement practice3 with async

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,21 +1,33 @@
-const getPoetry = () => {
-  return fetch("https://v1.jinrishici.com/all.json")
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error("Network response was not ok.");
-    })
-    .then((data) => {
-      const { origin, author, content } = data;
-      return [origin, author, content];
-    })
-    .catch((error) => {
-      throw new Error(
-        "There has been a problem with your fetch operation: ",
-        error.message
-      );
-    });
+// const getPoetryWithPromise = () => {
+//   return fetch("https://v1.jinrishici.com/all.json")
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error("Network response was not ok.");
+//     })
+//     .then((data) => {
+//       const { origin, author, content } = data;
+//       return [origin, author, content];
+//     })
+//     .catch((error) => {
+//       throw new Error(
+//         "There has been a problem with your fetch operation: ",
+//         error.message
+//       );
+//     });
+// };
+
+const getPoetryWithAsync = async function () {
+  const response = await fetch("https://v1.jinrishici.com/all.json");
+
+  if (response.ok) {
+    const { origin, author, content } = await response.json();
+    return [origin, author, content];
+  }
+
+  throw new Error("There are something error.");
 };
 
-export default getPoetry;
+// export default getPoetryWithPromise;
+export default getPoetryWithAsync;
